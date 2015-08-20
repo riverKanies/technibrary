221732b07d574a6d4f378873215c0335be4cb16f
River Kanies <river.kanies@gmail.com>
2015-08-20 16:20:16 -0400

    Add up nav button to nodes

--- technibrary.js
+++ technibrary.js
@@ -1,4 +1,6 @@
-
+//NOTES:
+	// I'm manually setting the child link data to null because it doesn't update if null otherwise
+		//see up method of node: solution is replaceState instead of setState
 
 var fb = new Firebase('https://technibrary2.firebaseio.com/');
 var fbList = fb.child('nodeList/');
@@ -15,11 +17,20 @@ var Node = React.createClass({
 	update: function(id) {
 		var nodePath = fbList.child(id+'/');
 		nodePath.once('value',function(snap){ // read fb data
-			//console.log(snap.val());
+			this.state.down = null;//is this really necessary???
+			console.log(snap.val().down);//this changes, but child data it's passed to doesn't... unless the obj exists
 			this.setState(snap.val()); // set react state
 		}.bind(this));
 
 		//!!!get children
+
+	},
+	up: function() {
+		var id = this.state.up;
+		var nodePath = fbList.child(id+'/');
+		nodePath.once('value',function(snap){ // read fb data
+			this.replaceState(snap.val()); // setState is an "update", replaceState clears previous
+		}.bind(this));
 
 	},
 	render: function() {
@@ -32,16 +43,21 @@ var Node = React.createClass({
 		//<p> Name: {this.state.name}</p>
 		//<p> Description: {this.state.desc}</p>
 
-		console.log('test dom ready');
+		console.log(this.state.down);
 
 		return (
 			<div>
 
 			      <div className="row">
 				    <div className="col s12 m6">
-
+				      <div className="row">
+				      	<div className="col s2 offset-s5">
+				    	  <a className="waves-effect waves-light btn" onClick={this.up} >^</a>
+				    	</div>
+			    	  </div>
 				      <div className="card blue-grey darken-1">
 				        <div className="card-content white-text">
+			        	  
 				          <span className="card-title">{this.state.name}</span>
 				          <p>{this.state.desc}</p>
 				        </div>
@@ -55,7 +71,17 @@ var Node = React.createClass({
 				    	  <ul className="collapsible" data-collapsible="accordion">
 						    <li>
 						      <div className="collapsible-header"><i className="material-icons">filter_drama</i>First</div>
-						      <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+						      <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p>
+								<a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
+								<ul id='dropdown1' class='dropdown-content'>
+								<li><a href="#!">one</a></li>
+								<li><a href="#!">two</a></li>
+								<li class="divider"></li>
+								<li><a href="#!">three</a></li>
+								</ul>
+						      </div>
+
+								
 						    </li>
 						    <li>
 						      <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
@@ -81,7 +107,8 @@ var Node = React.createClass({
 var ChildList = React.createClass({
 	//updateNode: function(key) {this.props.click(key);},
 	render: function() {
-		var list = this.props.data;
+		var list = this.props.data;//is this the problem?
+		console.log(list);//defined as object when no data passed!!!
 		var childNodes = [];
 		for (var key in list) {
 		   if (list.hasOwnProperty(key)) {
@@ -104,7 +131,7 @@ var Child = React.createClass({
 		//<button onClick={this.updateNode}>{this.props.id}</button>
 		return (
 			
-			<a className="waves-effect waves-light btn" onClick={this.updateNode} >{this.props.id}</a>
+			<a className="waves-effect waves-light btn" onClick={this.updateNode} ><i className="material-icons">trending_flat</i>{this.props.id}</a>
 		);
 	}
 });
